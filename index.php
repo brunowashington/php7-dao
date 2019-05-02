2f507563f8e92139a9e7d7535a2f42e8a9680694
Bruno Washington <brunow_santos@outlook.com>
2019-05-02 14:21:54 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <?php 
 	
 	require_once("config.php");
+	//carrega um usuario
+	//$root = new Usuario();
+	//$root->loadById(3);
+	//echo $root;
 
-	$root = new Usuario();
-	$root->loadById(3);
+	//Carrega uma lista de usuarios
+	//$lista = Usuario::getList();
+	//echo json_encode($lista);
 
-	echo $root;
+	//Carrega uma lista de usuarios buscando pelo login
+	//$search = Usuario::search("br");
+	//echo json_encode($search);
+
+	//Carrega um usuario usando o login e a senha
+	$usuario = new Usuario();
+	$usuario->login("bruno","123456789");
+
+	echo $usuario;
 
 
  ?>
